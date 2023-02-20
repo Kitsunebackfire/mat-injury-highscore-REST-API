297c6a04f13083633a2e24f710fb450c97961d72
Kitsunebackfire <kwolz92@gmail.com>
2023-02-20 09:38:43 -0600

    move mongodb config into mongoconfig.js, install mongodb-memory-server

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-
+require("./utilities/mongoConfig");
 const port = process.env.PORT || 3001;
 
 // mongodb
-mongoose.set("strictQuery", true);
-const mongoDB = process.env.DB_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 //routes
 const indexRouter = require("./routes/index.routes.js");
---
+++ controllers/__tests__/user.controller.test.js
@@ -0,0 +1,39 @@
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
+const userRouter = require("../../routes/user.routes");
+const createUser = jest.fn();
+
+describe("POST /users/register", () => {
+  describe("given a username, password, and email", () => {
+    test("should save username, password, and email to db", async () => {
+      const bodyData = [];
+    });
+    // should save username, password, and email to db
+    // should respond with json object containing user id
+
+    test("should respond with 201 status code", async () => {
+      request(userRouter)
+        .post("/register")
+        .send({
+          username: "tester",
+          password: "passwordtest",
+          email: "test@gmail.com",
+        })
+        .expect(201);
+    });
+
+    test("should specify json in the content type header", async () => {
+      request(userRouter)
+        .post("/register")
+        .send({
+          username: "tester",
+          password: "passwordtest",
+          email: "test@gmail.com",
+        })
+        .expect("Content-Type", /json/)
+        .expect(201);
+    });
+  });
+  //describe("when username and password is missing", () => {});
+});
